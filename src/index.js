ec680e19a8e9ed5945cbed3df461a36c98dae3bc
ChaulagainPujan <46419945+ChaulagainPujan@users.noreply.github.com>
2020-12-03 20:03:13 +0545

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
